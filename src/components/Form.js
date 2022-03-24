437761abf6e1db54f3f3d6e5f62dec1f103b74f2
ifeelblue99 <deadjoy01@gmail.com>
2022-03-24 16:08:54 +0300

    1 file modified

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,27 +1,60 @@
+import { application } from 'express';
 import React from 'react';
-import '../style';
-import encodeImage from '../lib/encodeImage';
+import '../style.css';
 
 export default function Form() {
   const [formData, setFormData] = React.useState({
-    imgUrl: '',
+    imageUrl: '',
     title: '',
     content: '',
     author: '',
   });
-  async function handleChange(element) {
-    const { name, value, type, files } = element.target;
-    const encodedUrl = await encodeImage(files[0]);
-    console.log(encodedUrl);
+  // encode to base64
+  function handleFileInput(event) {
+    let fileUrl = event.target.files[0];
+    const reader = new FileReader();
+    reader.onloadend = () => {
+      setFormData((prev) => {
+        return {
+          ...prev,
+          imageUrl: reader.result,
+        };
+      });
+    };
+    reader.readAsDataURL(fileUrl);
+  }
+  function handleChange(element) {
+    const { name, value } = element.target;
     setFormData((prevDdata) => {
       return {
         ...prevDdata,
-        name: value,
+        [name]: value,
       };
     });
-    console.log(formData);
   }
   function handleSubmit(event) {
+    let opt = {
+      title: formData.title,
+      content: formData.content,
+      author: formData.author,
+      imageurl: formData.imageUrl,
+    };
+    fetch('http://localhost:5500/post', {
+      method: 'POST',
+      headers: {
+        Accept: 'application/json',
+        'Content-Type': 'application/json',
+      },
+      body: formData,
+    }).then((res) => {
+      // setFormData({
+      //     imageUrl: '',
+      //     title: '',
+      //     content: '',
+      //     author: ''
+      // })
+      console.log(res);
+    });
     event.preventDefault();
   }
   return (
@@ -51,7 +84,7 @@ export default function Form() {
         max="15"
         value={formData.author}
       />
-      <input onchange={handleChange} type="file" />
+      <input onChange={handleFileInput} type="file" />
       <button>Submit Post</button>
     </form>
   );
